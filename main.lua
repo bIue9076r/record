7f3db93860b81e931b72c98efb80c7c450682456
blue <nobfi517@gmail.com>
2024-10-14 15:01:22 -0500

    Font Added

--- main.lua
+++ main.lua
@@ -11,11 +11,30 @@ require("ldb")
 focus = nil
 instring = ""
 
+BR = 0
+BG = 0x80/0xFF
+BB = 0x7F/0xFF
+TS = 14
+
+WinX = 15
+WinY = 15
+WinW = 800 - 30
+WinH = 600 - 30
+WinR = 0xD2/0xFF
+WinG = 0xD4/0xFF
+WinB = 0xD4/0xFF
+
 function love.load()
 	love.keyboard.setKeyRepeat(true)
 	for i,v in pairs(ldb) do
 		print(i,tostring(v))
 	end
+	
+	love.graphics.setBackgroundColor(BR,BG,BB)
+	font.newFont('main','/Fonts/W95FA.otf',TS)
+	love.graphics.setFont(font.getFont('main'))
+	
+	MainWindow = Window.new()
 end
 
 function love.update()
@@ -32,9 +51,21 @@ end
 
 function love.draw()
 	love.graphics.print("Hello World",250,300)
-	love.graphics.setColor({0.5,0.5,0.5})
-	love.graphics.rectangle("fill",0,0,250,300)
-	love.graphics.setColor({1,1,1})
-	love.graphics.rectangle("fill",50,50,100,50)
-	love.graphics.print({{0,0,0},instring},50,50)
+	
+	MainWindow.back:put(function()
+		love.graphics.setColor({WinR,WinG,WinB})
+		love.graphics.rectangle("fill",WinX,WinY,WinW,WinH)
+		love.graphics.setColor({1,1,1})
+	end)
+	
+	MainWindow.mid:put(function()
+		love.graphics.line(WinX,WinY,WinX + WinW,WinY)
+		love.graphics.line(WinX,WinY,WinX,WinY + WinH)
+		love.graphics.setColor({0,0,0})
+		love.graphics.line(WinX + WinW,WinY,WinX + WinW,WinY + WinH)
+		love.graphics.line(WinX,WinY + WinH,WinX + WinW,WinY + WinH)
+		love.graphics.setColor({1,1,1})
+	end)
+	
+	MainWindow()
 end
